9520d13cf3eeb7aecbaa932e75f8cd67d5ad752b
Serg Sizov <sergeyms@corepartners.ru>
2017-07-15 12:18:31 +0300

    initial state

---
+++ PingMon/MainFrm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PingMon
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "Ping: {0}";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblStatus.Location = new System.Drawing.Point(12, 9);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(105, 24);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Ping: {0}ms";
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Enabled = true;
+            this.mainTimer.Interval = 5000;
+            this.mainTimer.Tick += new System.EventHandler(this.mainTimer_Tick);
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 42);
+            this.Controls.Add(this.lblStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainFrm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Ping Monitor";
+            this.TopMost = true;
+            this.MinimumSizeChanged += new System.EventHandler(this.MainFrm_MinimumSizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer mainTimer;
+    }
+}
+
---
+++ PingMon/MainFrm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingMon
+{
+    public partial class MainFrm : Form
+    {
+        private Ping pingSender;
+        private Icon[] icons;
+
+        public MainFrm()
+        {
+            InitializeComponent();
+            this.Hide();
+            pingSender = new Ping();
+            icons = new Icon[] {
+                Properties.Resources.green,
+                Properties.Resources.yellow,
+                Properties.Resources.red
+            };
+            runPing();
+        }
+
+        private void runPing()
+        {
+            try
+            {
+                PingReply reply = pingSender.Send("8.8.8.8");
+                if (reply.Status == IPStatus.Success)
+                {
+                    long pingValue = reply.RoundtripTime;
+                    lblStatus.Text = String.Format("Ping: {0} ms", pingValue);
+                    notifyIcon.Text = lblStatus.Text;
+                    if (pingValue <= 70)
+                    {
+                        lblStatus.ForeColor = Color.Green;
+                        notifyIcon.Icon = icons[0];
+                    }
+                    else if (pingValue <= 160)
+                    {
+                        lblStatus.ForeColor = Color.DarkOrange;
+                        notifyIcon.Icon = icons[1];
+                    }
+                    else
+                    {
+                        lblStatus.ForeColor = Color.Red;
+                        notifyIcon.Icon = icons[2];
+                    }
+                }
+                else
+                {
+                    setUnavailableMode();
+                }
+            }
+            catch (Exception)
+            {
+                setUnavailableMode();
+            }
+        }
+
+        private void setUnavailableMode()
+        {
+            lblStatus.Text = "Unavailable";
+            notifyIcon.Text = lblStatus.Text;
+            lblStatus.ForeColor = Color.Red;
+            notifyIcon.Icon = icons[2];
+        }
+
+        private void mainTimer_Tick(object sender, EventArgs e)
+        {
+            runPing();
+        }
+
+        private void MainFrm_MinimumSizeChanged(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (!this.Visible)
+            {
+                this.Show();
+            } else
+            {
+                this.Hide();
+            }
+        }
+    }
+}
